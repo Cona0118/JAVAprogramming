8ba28c0f984c7a5e03ac916826ba1509a202564d
Cona0118 <127915993+Cona0118@users.noreply.github.com>
2025-10-16 15:42:16 +0900

    Add files via upload

---
+++ React/src/App.js
@@ -0,0 +1,23 @@
+/*
+import MyComponent from "./MyComponent";
+
+const App = () =>{
+  return <MyComponent favoriteNumber={1}>리액트</MyComponent>;
+};
+
+export default App;
+*/
+
+/*
+import Counter from './Counter';
+const App = () =>{
+  return <Counter />
+};
+export default App;
+*/
+
+import Say from './Say';
+const App = () => {
+  return <Say />
+};
+export default App;
