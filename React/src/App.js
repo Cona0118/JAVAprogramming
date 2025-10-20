48161a9242cecb2cb2e094b740552ff5bc2043ef
Cona0118 <127915993+Cona0118@users.noreply.github.com>
2025-10-20 18:16:01 +0900

    Add files via upload

--- React/src/App.js
+++ React/src/App.js
@@ -24,8 +24,50 @@ const App = () => {
 export default App;
 */
 
+/*
 import EventPractice from './EventPractice';
 const App = () => {
   return <EventPractice />
 };
+export default App;
+*/
+
+/*
+import { Component } from "react";
+import ValidationSample from "./ValidationSample";
+class App extends Component {
+  render(){
+    return (
+      <ValidationSample/>
+    );
+  }
+}
+export default App;
+*/
+
+/*
+import { Component } from "react";
+import ScrollBox from "./ScrollBox";
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox=ref} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>맨밑으로</button>
+      </div>
+    );
+  }
+}
+export default App;
+*/
+
+import { Component } from "react";
+import InteractionSample2 from "./InteractionSample2";
+class App extends Component {
+  render(){
+    return (
+      <InteractionSample2/>
+    );
+  }
+}
 export default App;
