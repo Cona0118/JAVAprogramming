3d03643609dfcc1ad045470b4df5a01c4b68acf5
Cona0118 <127915993+Cona0118@users.noreply.github.com>
2025-10-22 16:04:39 +0900

    Add files via upload

---
+++ React/todo-app/src/components/TodoInsert.js
@@ -0,0 +1,35 @@
+import {MdAdd} from 'react-icons/md';
+import './TodoInsert.scss';
+import { useCallback, useState } from 'react';
+
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            e.preventDefault(); // submit 이벤트가 브라우저 새로고침을 하지 않도록
+        },
+        [onInsert, value],
+    );
+
+    return (
+        <form className='TodoInsert' onSubmit={onSubmit}>
+            <input 
+                placeholder="할 일을 입력하세요" 
+                value={value} 
+                onChange={onChange}
+            />
+            <button type='submit'>
+                <MdAdd />
+            </button>
+        </form>
+    );
+};
+
+export default TodoInsert;
